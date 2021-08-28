9b1635851c5a5f71eb77fb9f3b7597696f85a661
Roy Rasugu <roy.rasugu@student.moringaschool.com>
2021-08-28 19:53:07 +0300

    add github API key

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiKey:'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'
 };
 
 /*
