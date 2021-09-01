e5c46224d5f5caca9f156c66ae8191f8fe6d3a92
Roy Rasugu <roy.rasugu@student.moringaschool.com>
2021-09-01 05:32:37 +0300

    put the necessary methods

--- src/app/github-service/github.service.ts
+++ src/app/github-service/github.service.ts
@@ -1,11 +1,37 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { User } from '../user-class/user';
+import { environment } from 'src/environments/environment';
+import { Observable} from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GithubService {
 
-  constructor() { 
-    console.log('Github Service Started....')
+  private username!: string;
+  private token = environment.apikey;
+  private clientId = environment.clientId;
+  private clientSecret = environment.clientSecret;
+  constructor(private http:HttpClient) { 
+  }
+  getProfileData(){
+    return this.http.get(`https://api.github.com/users/${this.username}?clientId=${this.clientId}&client_secret=${this.clientSecret}`);
+  }
+
+  getRepoData(){
+    return this.http.get(`https://api.github.com/users/${this.username}/repos?${this.clientId}&client_secret=${this.clientSecret}`);
+  }
+
+  getFollowers(){
+    return this.http.get(`https://api.github.com/users/${this.username}/followers?${this.clientId}&client_secret=${this.clientSecret}`);
+  }
+
+  getFollowing(){
+    return this.http.get(`https://api.github.com/users/${this.username}/following?${this.clientId}&client_secret=${this.clientSecret}`);
+  }
+
+  updateFields(username: string){
+    this.username = username;
   }
 }
