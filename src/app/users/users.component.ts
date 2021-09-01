be381bebf5837850ea41af0ae8616c00420cac52
Roy Rasugu <roy.rasugu@student.moringaschool.com>
2021-09-01 05:30:02 +0300

    put the method of submitInput

--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -6,7 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./users.component.css']
 })
 export class UsersComponent implements OnInit {
-
+  userProfile: any;
+  repos: any;
+  followers: any;
+  following: any;
+  username!: string;
   constructor() { }
 
   ngOnInit(): void {
